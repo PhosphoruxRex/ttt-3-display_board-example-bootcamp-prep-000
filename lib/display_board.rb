157078fb4840b31778fe64e3bb360ce2e32b6c6b
Lung Chen <lungheichen@gmail.com>
2018-07-14 13:09:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  cell = "   "
+  sep = "|"
+  lines = "-----------"
+  puts "#{cell}#{sep}#{cell}#{sep}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{sep}#{cell}#{sep}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{sep}#{cell}#{sep}#{cell}"
